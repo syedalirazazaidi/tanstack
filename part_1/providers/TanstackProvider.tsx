8e41993dbbd018c29ae5a2ab1f68d6869332cf7a
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2024-05-06 21:35:30 +0500

    add debugging tools

--- part_1/providers/TanstackProvider.tsx
+++ part_1/providers/TanstackProvider.tsx
@@ -1,13 +1,15 @@
+"use client";
 import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-const queryClient = new QueryClient();
 
 export default function TanstackProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const queryClient = new QueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
