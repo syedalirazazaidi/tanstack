c28e3aa5cac9f6870497034f20ea4248207f505a
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2024-05-06 22:35:26 +0500

    get data using react query

--- part_1/app/page.tsx
+++ part_1/app/page.tsx
@@ -1,10 +1,35 @@
-import Image from "next/image";
+"use client";
 import { Button } from "@/components/ui/button";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    },
+  });
+
+  if (isPending) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  const renderData = data.slice(0, 5).map((data: any) => {
+    return <div key={data.id}>{data.title}</div>;
+  });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hi developer
+      <div>{renderData}</div>
       <Button variant="ghost">Button</Button>
     </main>
   );
