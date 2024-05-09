a1b69ca0ad940ab98988bb06027efdeabd472484
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2024-05-09 22:56:49 +0500

    create new folder

--- forms/app/page.tsx
+++ forms/app/page.tsx
@@ -1,58 +1,11 @@
 "use client";
-import { Input } from "@/components/ui/input";
 
-import { useForm, SubmitHandler } from "react-hook-form";
-import { Schema } from "../types/schema";
-
-type Inputs = {
-  example: string;
-  exampleRequired: string;
-};
+import Forms from "../components/users/form";
 
 export default function Home() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<Schema>({ mode: "all" });
-  const onSubmit: SubmitHandler<Schema> = (data) => console.log(data);
-
-  console.log(watch("name"));
-
   return (
-    <form className=" " onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex justify-center items-center gap-4 mt-5">
-        <div className="flex flex-col">
-          <Input
-            className="w-48 border-blue-900"
-            defaultValue=""
-            {...register("name", {
-              required: { value: true, message: "The email is required" },
-            })}
-          />
-          <br />
-          {errors.name && <span>This field is required</span>}
-        </div>
-        <div className="flex flex-col">
-          <Input
-            className="w-48 border-blue-900"
-            defaultValue=""
-            {...register("email", {
-              required: { value: true, message: "The email is required" },
-            })}
-          />
-          <br />
-          {errors.email && <span>This field is required</span>}
-        </div>
-      </div>
-      <br />
-      <div className=" flex flex-1 justify-center items-center mx-auto container">
-        <input
-          type="submit"
-          className=" bg-red-300 px-8 p-1 rounded text-[11px] hover:cursor-pointer"
-        />
-      </div>
-    </form>
+    <>
+      <Forms />
+    </>
   );
 }
