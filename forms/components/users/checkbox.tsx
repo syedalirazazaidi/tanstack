e2876e63f2784be16517773ffd14e2bc865a3edc
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2024-05-16 23:20:08 +0500

    add new skills checkbox

--- forms/components/users/checkbox.tsx
+++ forms/components/users/checkbox.tsx
@@ -24,16 +24,19 @@ export function CheckboxBtn() {
       render={({ field: { onChange, value, ref }, fieldState: { error } }) => {
         console.log(error);
         return (
-          <div className="flex items-center space-x-2">
+          <div className="flex flex-col items-center space-x-2">
             <label
               htmlFor="terms"
               className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             >
-              Accept terms and conditions
+              Skills
             </label>
-            <Checkbox id="terms" />
-            <Checkbox id="terms" />
-            <Checkbox id="terms" />
+            <br />
+            <div className="flex justify-center gap-2">
+              <Checkbox id="terms" />
+              <Checkbox id="terms" />
+              <Checkbox id="terms" />
+            </div>
           </div>
         );
       }}
